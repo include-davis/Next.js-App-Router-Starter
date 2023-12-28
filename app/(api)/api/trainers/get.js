74012461021d00fc5c4dbe615f58525c231a955d
Austin Shih <austinhshih@gmail.com>
2023-12-28 15:05:01 -0800

    added projection for pokemon trainers

--- app/(api)/api/trainers/get.js
+++ app/(api)/api/trainers/get.js
@@ -8,7 +8,23 @@ export async function GET(request) {
     const queries = getQueries(request);
     const db = await dbConnect();
 
-    const trainer = await db.collection('trainers').find(queries).toArray();
+    const trainer = await db
+      .collection('trainers')
+      .aggregate([
+        {
+          $match: queries,
+        },
+        {
+          $lookup: {
+            from: 'pokemon',
+            localField: '_id',
+            foreignField: 'trainer_id',
+            as: 'pokemon',
+          },
+        },
+      ])
+      .project({ 'pokemon.trainer_id': 0 })
+      .toArray();
 
     return NextResponse.json({ ok: true, body: trainer }, { status: 200 });
   } catch (error) {

8859d31baf480349c2bf8f73cf18ebb74d233aa6
Austin Shih <austinhshih@gmail.com>
2024-02-11 17:45:27 -0800

    Added mongodb schema validation

--- app/(api)/_utils/mongodb/dbInit.mjs
+++ app/(api)/_utils/mongodb/dbInit.mjs
@@ -1,6 +1,6 @@
 import { getClient } from './mongoClient.mjs';
 import readline from 'readline';
-import collections from '../../_data/collections.json' with { type: 'json' };
+import schema from '../../_schema/index.mjs';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -23,9 +23,13 @@ async function dbInit(wipe) {
     console.log('\n');
 
     // Create collections if they don't already exist
-    for (const c of collections) {
-      await db.createCollection(c);
-      console.log(`Created collection: ${c}...`);
+    for (const [collection_name, collection_schema] of Object.entries(schema)) {
+      await db.createCollection(collection_name, {
+        validator: {
+          $jsonSchema: collection_schema,
+        },
+      });
+      console.log(`Created collection: ${collection_name}...`);
     }
     console.log('Created collections');
 
